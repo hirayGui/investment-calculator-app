cdf21ea3cc67c13f6dfd221187643138feb1b6ed
hiraygui <hiraygui@gmail.com>
2025-09-05 14:49:39 -0300

    feat(App): Implementando module AppModule para gerenciar todos os components utilizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,9 @@
-import { Component, signal } from '@angular/core';
-
-import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from './user-input/user-input.component';
-import { InvestmentResultsComponent } from './investment-results/investment-results.component';
-import { ResultsData } from './results-data.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
+  standalone: false,
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  resultsData = signal<ResultsData[] | undefined>(undefined);
-
-  
-  
 }
